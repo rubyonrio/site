867375d28ec93bab676eed4ba952614a8524cb3d
Marcello Milhomem Albuquerque <marcello.m.albuquerque@gmail.com>
2012-10-21 01:03:37 -0200

    Cleaned up config file comments

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
 Site::Application.config.secret_token = 'bbc514d0b3014a9a6a0a3b3183d447e0eb9a8bc8e910e6c386b84d5e0f42153f373a2bced128a5b645c5e6de55d922673d867010bb9ba5c1bfccf48995a4ebbb'
